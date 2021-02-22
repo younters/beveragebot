bd664902450aedb962acf37bbe8fc9b141b7b4ce
Younters <way1225@live.com>
2021-02-22 15:50:04 -0500

    Homepage and basic layout added

--- website/main.py
+++ website/main.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, flash, redirect, session, request, abort
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hello World!"
+    print(os.listdir ())
+    print("TEST")
+    return render_template('home.html')
 
 @app.route("/hello/<string:name>/")
 def hello(name):
@@ -11,5 +14,5 @@ def hello(name):
     './test.html',name=name)
 
 if __name__ == "__main__":
-   app.run()
+   app.run(debug = True)
 
